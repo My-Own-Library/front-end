b1d1f27871cbf2ecca094a6c0e4e8e1a43405278
Rodrigo Antunes <rodrigo.teth@gmail.com>
2023-03-28 23:36:24 -0300

    feat: page route test

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+
+export default function SigninPage(){
+
+  return (
+    <SignInPage>
+
+      
+    </SignInPage>
+  )
+  
+}
+
+const SignInPage = styled.main`
+  
+
+`
